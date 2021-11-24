a30efed0a525c45dfb41ce6688f7528e96aba018
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-24 22:22:47 +0100

    Bio changed

--- src/displayComponents/GoodHeader.js
+++ src/displayComponents/GoodHeader.js
@@ -2,8 +2,8 @@
 const GoodHeader = (params) => {
   return (
     <div className="text-left w-min whitespace-nowrap">
-      <h1 className="text-gray-50 font-bold">{params.title}</h1>
-      <div className="h-1.5 bg-gray-500"></div>
+      <h1 className="text-gray-50 font-bold mb-1">{params.title}</h1>
+      <div className="h-1 bg-gray-500"></div>
     </div>
   )
 }
--- src/displayComponents/bioDisplay.js
+++ src/displayComponents/bioDisplay.js
@@ -1,24 +1,44 @@
 import Image from "next/image"
-import {GoodHeader} from "../index"
+import { GoodHeader } from "../index"
 
 const BioDisplay = () => {
-  return (
-    <div className="">
-      <div className="flex items-center">
-        <div className="text-left mr-8">
-          <h1 className="text-white text-2xl">Gijs Bakker</h1>
-          <p className="text-white">Full-Stack programmer</p>
-        </div>
-        <div>
-          <Image src="/pf.jpg" width="100" height="100" className="rounded-full" />
-        </div>
-      </div>
-      <GoodHeader title='My Profile' />
-      <p>Hello! my name is Gijs</p>
-      <GoodHeader title='Bio' />
-      <p>Hello! my name is Gijs</p>
-    </div>
-  )
+
+	const BioLine = (data) => {
+		return (
+			<div className="flex">
+				<h3 className="text-white mr-4">{data.data.year}</h3>
+				<p className="text-white">{data.data.text}</p>
+			</div>
+		)
+	}
+
+	return (
+		<div className="mt-10">
+			<div className="flex items-center">
+				<div className="text-left mr-8">
+					<h1 className="text-white text-2xl">Gijs Bakker</h1>
+					<p className="text-white">Full-Stack programmer</p>
+				</div>
+				<div>
+					<Image src="/pf.jpg" width="100" height="100" className="rounded-full" />
+				</div>
+			</div>
+			<div className="mt-10">
+				
+				<GoodHeader title='Bio' />
+					{
+						BioData.map((data) => <BioLine data={data}/>)
+					}
+			</div>
+		</div>
+	)
 }
+
+const BioData = [
+	{
+		year: 2001,
+		text: "Born"
+	}
+]
 
 export default BioDisplay;
