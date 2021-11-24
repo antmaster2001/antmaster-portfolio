91504f81f0ef040ead8054fc887b79aa0e59d31e
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-24 22:22:29 +0100

    My profile

---
+++ src/displayComponents/MyProfile.js
@@ -0,0 +1,12 @@
+import { GoodHeader } from "../index"
+
+const MyProfile = () => {
+	return (
+		<div>
+			<GoodHeader title='My Profile' />
+			<p className="text-white">Hello! my name is Gijs</p>
+		</div>
+	)
+}
+
+export default MyProfile;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Footer from "./displayComponents/Footer"
 import Nav from "./displayComponents/Nav"
 import BioDisplay from "./displayComponents/bioDisplay"
+import MyProfile from "./displayComponents/MyProfile"
 import GoodHeader from "./displayComponents/GoodHeader"
 
-export {Footer, Nav, BioDisplay, GoodHeader}
+export {Footer, Nav, BioDisplay, GoodHeader, MyProfile}
