cbf272416b90c8d57c24da6bee9a4519c2162adc
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-24 22:22:58 +0100

    Title update

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
-import {Footer, Nav, BioDisplay} from "../src"
+import {Footer, Nav, BioDisplay, MyProfile} from "../src"
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen py-2 container mx-auto">
       <Head>
-        <title>Create Next App</title>
+        <title>antmaster2001 - Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col w-full flex-1 px-20 min-h-screen">
         <Nav />
+        <MyProfile />
         <BioDisplay />
       </main>
 
