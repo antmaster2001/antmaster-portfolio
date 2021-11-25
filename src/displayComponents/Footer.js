224a31e3df4285d971f5117be854fbffcad3e2e1
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-25 02:36:24 +0100

    Footer and nav update

--- src/displayComponents/Footer.js
+++ src/displayComponents/Footer.js
@@ -1,11 +1,32 @@
-export default function Footer () {
-  return (
-    <footer className="flex flex-row" id="about">
-      <div className="">
-        <a href="mailto:gijsbakker@hotmail.nl">
-          <h3>gijsbakker@hotmail.nl</h3>
-        </a>
-      </div>
-    </footer>
-  )
+export default function Footer() {
+	return (
+		<footer className=" pt-20 px-6 w-full text-center justify-center" >
+			<div className="w-full">
+				<div className="w-full md:flex block">
+					<div className="md:mr-4 ml-0">
+						<p className="text-white mt-4">Email: </p>
+						<a href="mailto:gijsbakker@hotmail.nl">
+							<h3 className="text-white hover:text-blue-500">gijsbakker@hotmail.nl</h3>
+						</a>
+					</div>
+					<div className="md:mr-4 ml-0">
+						<p className="text-white mt-4">LinkedIn: </p>
+						<a href="https://www.linkedin.com/in/gijs-bakker-2001/">
+							<h3 className="text-white hover:text-blue-500">@GijsBakker</h3>
+						</a>
+					</div>
+					<div className="md:mr-4 ml-0">
+						<p className="text-white mt-4">GitHub: </p>
+						<a href="https://github.com/antmaster2001">
+							<h3 className="text-white hover:text-blue-500">@antmaster2001</h3>
+						</a>
+					</div>
+
+				</div>
+			</div>
+			<div className="w-full my-5">
+				<p className="text-gray-500">© 2021 antmaster2001. All Rights Reserved.</p>
+			</div>
+		</footer>
+	)
 }
--- src/displayComponents/Nav.js
+++ src/displayComponents/Nav.js
@@ -1,19 +1,48 @@
 import Image from "next/image"
+import { useState } from "react"
 
 export default function Nav() {
-  return (
-    <div className="flex justify-between pt-5">
-      <a href="/" className="flex">
-        <h3 className="text-gray-50 font-bold">GIJS BAKKER</h3>
-      </a>
-      <div className="flex">
-        <a href="#about" className="mx-3">
-          <h3 className="text-gray-50 font-bold text-lg"> Over</h3>
-        </a>
-        <a href="/projects" className="mx-3">
-          <h3 className="text-gray-50 font-bold text-lg">Projecten</h3>
-        </a>
-      </div>
-    </div>
-  )
+	const [hamMenu, sethamMenu] = useState(false);
+	return (
+		<nav className="flex justify-between pt-5 md:px-20 px-6">
+			<a href="/" className="flex">
+				<h3 className="text-gray-50 font-bold">GIJS BAKKER</h3>
+			</a>
+			<div className="hidden md:flex">
+				<a href="/#about" className="mx-3">
+					<h3 className="text-gray-50 font-bold text-lg"> About</h3>
+				</a>
+				<a href="/projects" className="mx-3">
+					<h3 className="text-gray-50 font-bold text-lg">Projecten</h3>
+				</a>
+			</div>
+			<div onClick={() => sethamMenu((e) => !e)} className="md:hidden block">
+				<svg class="block h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white" aria-hidden="true">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+				</svg>
+			</div>
+
+			{hamMenu ?
+				<div className="z-50 min-h-screen max-h-screen bg-black absolute top-0 min-w-full max-w-max left-0 pt-5 px-6">
+					<div className="flex justify-between pt-2 md:hidden" onClick={() => sethamMenu((e) => !e)}>
+						<div></div>
+						<svg class="block h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white" aria-hidden="true">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</div>
+					<div className="">
+						<a href="/">
+							<p className="text-white mt-5 text-xl">Home</p>
+						</a>
+						<a href="/#about" onClick={() => sethamMenu(false)}> 
+							<p className="text-white mt-5 text-xl">About</p>
+						</a>
+						<a href="/projects">
+							<p className="text-white mt-5 text-xl">Projecten</p>
+						</a>
+					</div>
+				</div> :
+				<></>}
+		</nav>
+	)
 }
