6bcc6270fe0fded0aebc7d2a0536ac40af127f9f
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-25 14:37:53 +0100

    Added more Technologies

--- src/displayComponents/Technologies.js
+++ src/displayComponents/Technologies.js
@@ -13,12 +13,25 @@ const Technologies = () => {
 				<li className="text-white">GraphQL</li>
 				<li className="text-white">MySQL</li>
 				<li className="text-white">MongoDB</li>
+				<li className="text-white">Web Scraping</li>
+				<li className="text-white">Web Extension Programming</li>
+				<li className="text-white">Run.db/SmartContracts</li>
 			</ul>
 			<p className="text-gray-300 mb-5 mt-8">Most used languages:</p>
 			<ul className="list-inside list-disc">
 				<li className="text-white">Typescript</li>
 				<li className="text-white">Javascript</li>
 				<li className="text-white">Python</li>
+			</ul>
+			<p className="text-gray-300 mb-5 mt-8">Other:</p>
+			<ul className="list-inside list-disc">
+				<li className="text-white">VIM</li>
+				<li className="text-white">HTML</li>
+				<li className="text-white">JSON</li>
+				<li className="text-white">PHP</li>
+				<li className="text-white">Logo Design</li>
+				<li className="text-white">Mobile Design</li>
+				<li className="text-white">Web Design</li>
 			</ul>
 		</div>
 	)
