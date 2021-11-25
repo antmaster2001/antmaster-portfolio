95717873e8d68517b4ccfbc6a74281b2a31fbfa8
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-25 02:36:33 +0100

    GoodHeader update

--- src/displayComponents/GoodHeader.js
+++ src/displayComponents/GoodHeader.js
@@ -1,7 +1,7 @@
 
 const GoodHeader = (params) => {
   return (
-    <div className="text-left w-min whitespace-nowrap">
+    <div className="text-left w-min whitespace-nowrap mb-3">
       <h1 className="text-gray-50 font-bold mb-1">{params.title}</h1>
       <div className="h-1 bg-gray-500"></div>
     </div>
