49e47e2c3b28785f621154bc452ba5463576002f
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-25 02:35:52 +0100

    Components added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import Footer from "./displayComponents/Footer"
 import Nav from "./displayComponents/Nav"
 import BioDisplay from "./displayComponents/bioDisplay"
 import MyProfile from "./displayComponents/MyProfile"
+import UserPfShow from "./displayComponents/UserPfShow"
 import GoodHeader from "./displayComponents/GoodHeader"
+import HobbiesDisplay from "./displayComponents/HobbiesDisplay"
+import Technologies from "./displayComponents/Technologies"
 
-export {Footer, Nav, BioDisplay, GoodHeader, MyProfile}
+export {Footer, Nav, BioDisplay, GoodHeader, MyProfile, UserPfShow, HobbiesDisplay, Technologies}
