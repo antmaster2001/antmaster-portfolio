7ffd8d0ff4c6c45038fe3289d823ced16ba7082e
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-25 02:35:44 +0100

    Upgraded Projects display

--- pages/projects.js
+++ pages/projects.js
@@ -10,14 +10,14 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
+			<Nav />
 			<main className="flex flex-col w-full flex-1 px-20 min-h-screen">
-				<Nav />
-				<div className='flex md:justify-center w-100'><div className='md:w-6/12'><h1 className='text-lg text-white'>Projects</h1></div></div>
+				<div className='flex md:justify-center w-100 mt-10'><div className='md:w-6/12 mb-4'><h1 className='text-lg text-white'>Projects</h1></div></div>
 				<section className='flex justify-center w-100'>
 					<article className='grid md:grid-cols-2 md:w-6/12 w-100'>
-						{projects.map(data => (
+						{projects.map((data, index) => (
 							<a href={data.link}>
-								<div className='flex flex-col items-center mb-10 text-center hover:bg-gray-800 md:p-11 rounded'>
+								<div className='flex flex-col items-center mb-10 text-center hover:bg-gray-800 md:p-11 rounded' key={index}>
 									<Image src={"/" + data.img} width={600} height={400} className="rounded" />
 									<h3 className='text-gray-300 text-lg font-bold'>{data.name}</h3>
 									<p className='text-gray-300 mt-2'>{data.desc}</p>
@@ -45,11 +45,5 @@ const projects = [
 		img: "FeelGoodDesign.png",
 		link: "feelgoodradio",
 		desc: "A IOS/ANDROID app deployed in stores"
-	},
-	{
-		name: "Fast & Fresh",
-		img: "mexicaannoordwijk.png",
-		link: "mexicaannoordwijk",
-		desc: "Website from local business"
 	}
 ]
