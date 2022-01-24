50d3e07fd678693002b09ea1ccc460e7136354f1
antmaster2001 <gijsbakker@hotmail.nl>
2022-01-24 19:38:58 +0100

    Technologies from text to map

--- src/displayComponents/Technologies.js
+++ src/displayComponents/Technologies.js
@@ -6,38 +6,56 @@ const Technologies = () => {
 			<GoodHeader title='Technologies' />
 			<p className="text-gray-300 mb-5">These are some technologies I'm experienced in or have a very big interest for:</p>
 			<ul className="list-inside list-disc">
-				<li className="text-white">React</li>
-				<li className="text-white">React Native</li>
-				<li className="text-white">Electron</li>
-				<li className="text-white">Node.JS</li>
-				<li className="text-white">Express.JS</li>
-				<li className="text-white">GraphQL</li>
-				<li className="text-white">MySQL</li>
-				<li className="text-white">MongoDB</li>
-				<li className="text-white">Web Scraping</li>
-				<li className="text-white">Web Extension Programming</li>
-				<li className="text-white">Run.db/SmartContracts</li>
+				{Tech["Main"].map(data => (
+					<li className="text-white">{data}</li>
+				))}
 			</ul>
 			<p className="text-gray-300 mb-5 mt-8">Most used languages:</p>
 			<ul className="list-inside list-disc">
-				<li className="text-white">Typescript</li>
-				<li className="text-white">Javascript</li>
-				<li className="text-white">Python</li>
+				{Tech["Lang"].map(data => (
+					<li className="text-white">{data}</li>
+				))}
 			</ul>
 			<p className="text-gray-300 mb-5 mt-8">Other:</p>
 			<ul className="list-inside list-disc">
-				<li className="text-white">VIM</li>
-				<li className="text-white">Tailwind CSS</li>
-				<li className="text-white">Bootstrap</li>
-				<li className="text-white">HTML</li>
-				<li className="text-white">JSON</li>
-				<li className="text-white">PHP</li>
-				<li className="text-white">Logo Design</li>
-				<li className="text-white">Mobile Design</li>
-				<li className="text-white">Web Design</li>
+				{Tech["Other"].map(data => (
+					<li className="text-white">{data}</li>
+				))}
 			</ul>
 		</div>
 	)
+}
+
+const Tech = {
+	Main: [
+		"React",
+		"React Native",
+		"Electron",
+		"Node.JS",
+		"Express.JS",
+		"GraphQL",
+		"MySQL",
+		"MongoDB",
+		"Web Scraping",
+		"Web Extension Programming",
+		"Run.db/SmartContracts",
+	],
+	Lang: [
+		"Typescript",
+		"Javascript",
+		"Python",
+	],
+	Other: [
+		"VIM",
+		"Tailwind CSS",
+		"Bootstrap",
+		"HTML",
+		"JSON",
+		"PHP",
+		"Logo Design",
+		"Mobile Design",
+		"Web Design",
+	]
 }
 
 export default Technologies;
